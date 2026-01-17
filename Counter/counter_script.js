4675adec0f1e51a916ed7d265208c35200dd01b1
Shweta Bora <222091288+sbora316@users.noreply.github.com>
2026-01-17 18:11:23 -0500

    added counter app

---
+++ Counter/counter_script.js
@@ -0,0 +1,23 @@
+
+const decreasebtn = document.getElementById("decreasebtn");
+const resetbtn = document.getElementById("resetbtn");
+const increasebtn = document.getElementById("increasebtn");
+const countLabel = document.getElementById("countlabel");
+let count = 0;
+
+increasebtn.onclick=function(){
+    count++
+    countLabel.textContent=count
+};
+
+decreasebtn.onclick= function(){
+    count--
+    countLabel.textContent= count
+};
+
+resetbtn.onclick= function(){
+    count=0
+    countLabel.textContent=count
+
+};
+
