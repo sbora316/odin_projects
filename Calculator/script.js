3e983819b5a88a9d7d4609393d86f62d4f3b9945
Shweta Bora <222091288+sbora316@users.noreply.github.com>
2026-01-22 12:46:40 -0500

    Calculator

---
+++ Calculator/script.js
@@ -0,0 +1,16 @@
+// calculator
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = '';
+}
+
+function calculate(){
+    display.value= eval(display.value);
+}
+
